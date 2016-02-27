842842ff54725a7934de2c52d2c73cc2ddd419cd
cjarnaker <chris@proxmea.com>
2016-02-27 18:02:11 +0100

    Initial release

---
+++ Program.cs
@@ -0,0 +1,203 @@
+﻿// Copyright(c) 2016 Christoffer Järnåker
+// License and source: https://github.com/jrnker/BumpVersions
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace bumpversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args[0];
+
+            bool major = false;
+            bool minor = false;
+            bool build = false;
+            bool revision = false;
+
+            bool reset = false;
+
+            foreach (var a in args)
+            {
+                // Parse the options
+                switch (a)
+                {
+                    // Will bump the respective part with +1
+                    case "-major":
+                        major = true;
+                        break;
+                    case "-minor":
+                        minor = true;
+                        break;
+                    case "-build":
+                        build = true;
+                        break;
+                    case "-revision":
+                        revision = true;
+                        break;
+
+                    // Will set all after the mark to zero. E.g. v1.2.3.4 with a -build -reset will become v1.2.4.0.
+                    // You normally want this
+                    case "-reset":
+                        reset = true;
+                        break;
+
+                    // Display help
+                    case "-?":
+                    case "-h":
+                    case "--help":
+                        Console.WriteLine(string.Format("{0}, v{1}", Assembly.GetExecutingAssembly().GetName().Name.ToString(), Assembly.GetExecutingAssembly().GetName().Version.ToString()));
+                        Console.WriteLine("License and source: https://github.com/jrnker/BumpVersions");
+                        Console.WriteLine(@"
+Usage:
+  bumpversions.exe [pathtosearch] [-major|-minor|-build|-revision] [-reset]
+  Note: if submitting a path with quotation marks, e.g. a path with spaces, then place space between the end of the path and the second quotation mark.
+
+Example:
+  bumpversions.exe C:\repo\BumpVersions\ -build -reset");
+                        return;
+                }
+            }
+
+            // Check if submitted path exists
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine(string.Format("Directory {0} doesn't exist.", path));
+                return;
+            }
+
+            // Find all assemblyinfo files
+            var files = findAllFiles(path.Trim());
+
+            // Array of what to search for and try to bump
+            // First two are for C#, last two for VB.Net
+            string[] prefixes = new string[] { "[assembly: AssemblyVersion(\"", "[assembly: AssemblyFileVersion(\"",
+            "<Assembly: AssemblyVersion(\"", "<Assembly: AssemblyFileVersion(\""};
+
+            // Iterate through the found files
+            foreach (var f in files)
+            {
+                bool fileUpdated = false;
+                try
+                {
+                    string data = "";
+                    List<string> sresult = new List<string>();
+
+                    // Read the file
+                    using (StreamReader sr = new StreamReader(f))
+                    {
+                        data = sr.ReadToEnd();
+                    }
+
+                    // Search for matching part to bump
+                    foreach (string s in data.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        var d = s;
+                        foreach (string p in prefixes)
+                        {
+                            if (s.Length > p.Length && s.Substring(0, p.Length) == p)
+                            {
+                                // C#
+                                //[assembly: AssemblyVersion("1.0.0.0")]
+                                //[assembly: AssemblyFileVersion("1.0.0.0")]
+
+                                // vb.net
+                                //<Assembly: AssemblyVersion("2.6.0.0")>
+                                //<Assembly: AssemblyFileVersion("2.6.0.0")>
+
+                                // Set the ending of the string if we edit it.
+                                // C# ends with ] while VB.Net ends with >
+                                string postfix = "\")" + (p[0] == '[' ? "]" : ">");
+
+                                Version v = null;
+                                if (Version.TryParse(d.Replace(p, "").Split('"')[0], out v))
+                                {
+                                    int imajor = v.Major;
+                                    int iminor = v.Minor;
+                                    int ibuild = v.Build;
+                                    int irevision = v.Revision;
+
+                                    if (revision)
+                                    {
+                                        irevision += 1;
+                                    }
+                                    if (build)
+                                    {
+                                        ibuild += 1;
+                                        if (reset)
+                                        {
+                                            irevision = 0;
+                                        }
+                                    }
+                                    if (minor)
+                                    {
+                                        iminor += 1;
+                                        if (reset)
+                                        {
+                                            ibuild = 0;
+                                            irevision = 0;
+                                        }
+                                    }
+                                    if (major)
+                                    {
+                                        imajor += 1;
+                                        if (reset)
+                                        {
+                                            iminor = 0;
+                                            ibuild = 0;
+                                            irevision = 0;
+                                        }
+                                    }
+                                    // Rebuild this line with the new version info
+                                    d = p + new Version(imajor, iminor, ibuild, irevision) + postfix;
+                                    fileUpdated = true;
+                                }
+                            }
+                        }
+                        // Add it to the rebuild file list
+                        sresult.Add(d);
+                    }
+
+                    // If we've been busy, then write the file back with the information
+                    if (fileUpdated)
+                    {
+                        Console.WriteLine(string.Format("Updated {0}", f));
+                        using (StreamWriter sr = new StreamWriter(f))
+                        {
+                            foreach (var l in sresult)
+                                sr.WriteLine(l);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format(" FAILED with error {0}", ex.Message));
+                }
+            }
+        }
+        /// <summary>
+        /// Find all AssemblyInfo files in a particular path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>List of full paths to the found files</returns>
+        private static List<string> findAllFiles(string path)
+        {
+            List<string> files = new List<string>();
+            var find = "AssemblyInfo.*";
+
+            foreach (var f in Directory.EnumerateFiles(path, find))
+            {
+                files.Add(f);
+            }
+            foreach (var d in Directory.EnumerateDirectories(path))
+            {
+                files.AddRange(findAllFiles(d));
+            }
+            return files;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # BumpVersions
 Small tool to bump the version numbers of your project
+License and source: https://github.com/jrnker/BumpVersions
+
+### Description
+It will search the submitted path for AssemblyInfo.* files, parse these and increase version numbers as specified.
+
+It's meant to be implemented into your Visual Studio projects build flow to organize your executables and such with proper version numbers.
+
+Built on .Net 4.5.2
+
+### Usage
+  bumpversions.exe [pathtosearch] [-major|-minor|-build|-revision] [-reset]
+  
+  Note: if submitting a path with quotation marks, e.g. a path with spaces, then place space between the end of the path and the second quotation mark.
+
+Example with normal path:
+```sh
+  bumpversions.exe C:\repo\BumpVersions\ -build -reset
+```
+Example with path with spaces:
+```sh
+  bumpversions.exe "C:\my repo\BumpVersions\ " -build -reset
+```
+
+### Pre compiled version
+..can be found in https://github.com/jrnker/BumpVersions/bin/release
+
+### Implement in your project (C#)
+* Open your project settings and go to Build Events
+* In *Pre-build event command line:*, insert the below code, with adjusted paths to buildevents.exe
+
+Pre-build event command line:
+```sh
+if "$(ConfigurationName)"=="Debug" Goto Debug
+if "$(ConfigurationName)"=="Release" Goto Release
+Goto End
+
+:Debug
+C:\repo\Distribution\tools\bumpversions.exe "$(ProjectDir) " -revision -reset
+Goto End
+
+:Release
+C:\repo\Distribution\tools\bumpversions.exe "$(ProjectDir) " -build -reset
+Goto End
+
+:End
+```
+
+With this configuration, your project will increase like this:
+For debug mode, v1.0.0.0, v1.0.0.1, v1.0.0.2
+For release mode, v1.0.0.0, v1.0.1.0, v1.0.2.0
